967edf65dd0607eb0e4b125eda6aed52774f9b50
root <root@ip-172-31-28-6.ap-south-1.compute.internal>
2025-06-17 11:45:45 +0000

    angular-commit2

--- message-app/src/app/message-form/message-form.component.ts
+++ message-app/src/app/message-form/message-form.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-message-form',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, HttpClientModule],
   templateUrl: './message-form.component.html',
   styleUrls: ['./message-form.component.css']
 })
@@ -15,12 +16,28 @@ export class MessageFormComponent {
   post = '';
   submittedMessages: { name: string; email: string; post: string }[] = [];
 
+  constructor(private http: HttpClient) {}
+
   onSubmit() {
     if (this.name && this.email && this.post) {
-      this.submittedMessages.push({ name: this.name, email: this.email, post: this.post });
-      this.name = '';
-      this.email = '';
-      this.post = '';
+      const message = {
+        name: this.name,
+        email: this.email,
+        post: this.post
+      };
+
+      this.http.post('http://3.6.205.240:3000/messages', message).subscribe({
+        next: () => {
+          console.log('Message sent to backend');
+          this.submittedMessages.push(message); // Optional: For frontend display
+          this.name = '';
+          this.email = '';
+          this.post = '';
+        },
+        error: (err) => {
+          console.error('Error submitting message:', err);
+        }
+      });
     }
   }
 }
